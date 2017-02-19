3f96d699f8e8a7e5b2cc9c3c8ad53de0523e0c6a
Pablo <pablopbio@pasodiges.com>
2017-02-19 12:40:59 +0100

    Working until parsing organism ncbi db number

--- src/ProteinParser.py
+++ src/ProteinParser.py
@@ -11,9 +11,10 @@ class ProteinParser(object) :
     def __init__(self, fileHandler):
         self.fileHandler = fileHandler
         self.attributeGeneral = '^\s{0,2}[A-Z]+\s [A-Z]*'
-        self.fieldsToParse = ["id", "name"]#, "org", "links", "seq"]
+        self.fieldsToParse = ["id", "name", "org"]#, "org", "links", "seq"]
         self.fieldsRegExp = {"id"   : "LOCUS\s*(.*)",
-                             "name" : "DEFINITION\s*(.*)"}
+                             "name" : "FEATURES\s*(.*)",
+                             "org" : "FEATURES\s*(.*)"}
 
     def __iter__(self):
         return self
@@ -23,39 +24,53 @@ class ProteinParser(object) :
         return result.split()[0]
 
     def getName(self, matches):
-        result = re.search("(hypothetical protein)? (.*)(\, partial)? \[.*\].", matches.group(1))
+        result = re.search("gene=\"(.*?)\"",matches.group(1))
         if result is not None:
-            return result.group(2)
+            return result.group(1)
+        else:
+            return '-'
 
+    def getOrganism(self, matches):
+        result = re.search("taxon:(.*?)\"",matches.group(1))
+        if result is not None:
+            return result.group(1)
+        else:
+            return '-'
 
     def next(self):
-        valuesDict = {'id':'not available', 'name':'not available'}#, 'org':'not available', 'links':'not available',
-                      #'seq':'not available'}
+        valuesDict = {'id':'-', 'name':'-', 'org':'-'}#, 'links':'-',
+                      #'seq':'-'}
 
         isGeneraltt = False
-        readingValue = True
-        valueGotten = False
         value = ""
+        readingAtt = False
+        firstAtt = True
         for line in self.fileHandler:
             line = line.rstrip('\n')
             if re.search('^//', line) is not None:
                 return valuesDict
+            if firstAtt:
+                value = line
+                firstAtt = False
+                continue
             # check if we are at the star line of a new attribute
             regAtExp = re.compile(self.attributeGeneral)
             isGeneraltt = regAtExp.match(line)
+
             if isGeneraltt:
-                if readingValue:
-                    value = line
-                    valueGotten = True
-                    value.rstrip('\n')
-                    for field in self.fieldsToParse:
-                        matches = re.search(self.fieldsRegExp[field], value)
-                        if matches is not None:
-                            if field == "id":
-                                valuesDict[field] = self.getId(matches)
-                            elif field == "name":
-                                valuesDict[field] = self.getName(matches)
+                for field in self.fieldsToParse:
+                    matches = re.search(self.fieldsRegExp[field], value)
+                    if matches is not None:
+                        if field == "id":
+                            valuesDict[field] = self.getId(matches)
+                            break
+                        if field == "name":
+                            valuesDict[field] = self.getName(matches)
+                        if field == "org":
+                            valuesDict[field] = self.getOrganism(matches)
+                value = line
             else:
+                readingAtt = True
                 value += line
 
         raise StopIteration
@@ -85,7 +100,7 @@ def checkParams():
 
 if __name__ == '__main__':
 
-    try :
+    try:
         checkParams()
     except OSError:
         print 'File ', sys.argv[1], ' doesn\'t exists'
@@ -96,12 +111,15 @@ if __name__ == '__main__':
 
     except StandardError:
         print ('Usage: python ProteinParser origin_file output_tab_file output_FASTA_file')
+        exit()
 
     fHandler = open(sys.argv[1], 'r')
-    valuesDict = {'id':'not available'}#, 'name':'not available', 'org':'not available', 'links':'not available',
-              #    'seq':'not available'}
+    valuesDict = {'id':'-', 'name':'-', 'org':'-'}#, 'links':'-',
+              #    'seq':'-'}
     for valuesDict in ProteinParser(fHandler):
+        a = 1
         print "id: ", valuesDict['id']
         print "gene name: ", valuesDict['name']
+        print "organism: ", valuesDict['org']
 
 
